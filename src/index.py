50c108bc254ef7eb4fb62a4f800eb7d1c951f95e
RodrigoIgnacio95 <65138058+RodrigoIgnacio95@users.noreply.github.com>
2020-06-09 11:54:44 -0400

    pequeños cambios
    ingreso de productos, inicio de sesion charcha

--- src/index.py
+++ src/index.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+#inicio de sesion
+@app.route('/inicio-sesion')
+def iniciosesion():
+    return render_template('/inicio-sesion.html')
+
 #ruta del registro de cliente
 @app.route('/registro-cliente')
 def registroCliente():
@@ -16,6 +21,11 @@ def registroCliente():
 @app.route('/registro-productor')
 def registroProductor():
     return render_template('registro-productor.html')
+
+#ruta del ingreso de productos
+@app.route('/ingreso-productos')
+def ingresoProductos():
+    return render_template('ingreso-productos.html')    
 
 if __name__ == "__main__":
     app.run(debug=True)
