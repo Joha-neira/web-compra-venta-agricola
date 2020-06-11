4a31959a9012f9dac4d5292a8e2f8d99f58ac3f0
RodrigoIgnacio95 <65138058+RodrigoIgnacio95@users.noreply.github.com>
2020-06-11 12:43:05 -0400

    historial de compras y favoritos
    historial de compras y favoritos

--- src/index.py
+++ src/index.py
@@ -25,7 +25,17 @@ def registroProductor():
 #ruta del ingreso de productos
 @app.route('/ingreso-productos')
 def ingresoProductos():
-    return render_template('ingreso-productos.html')    
+    return render_template('ingreso-productos.html')   
+
+#ruta de favoritos
+@app.route('/favoritos')
+def favoritos():
+    return render_template('favoritos.html')    
+
+#ruta de compras
+@app.route('/compras')
+def compras():
+    return render_template('compras.html')        
 
 if __name__ == "__main__":
     app.run(debug=True)
